a5e716c7f2840b47ac4c9c5b12f4a0f715e777ec
skustov <skustov@clevertec.ru>
2024-10-27 13:56:20 +0300

    chore(*): add packages and config dev mode

--- src/http_server/index.ts
+++ src/http_server/index.ts
@@ -1,11 +1,11 @@
-import fs from 'fs';
-import path from 'path';
-import http from 'http';
+import { readFile } from 'fs';
+import { resolve, dirname } from 'path';
+import { createServer } from 'http';
 
-export const httpServer = http.createServer(function (req, res) {
-  const __dirname = path.resolve(path.dirname(''));
+export const httpServer = createServer(function (req, res) {
+  const __dirname = resolve(dirname(''));
   const file_path = __dirname + (req.url === '/' ? '/front/index.html' : '/front' + req.url);
-  fs.readFile(file_path, function (err, data) {
+  readFile(file_path, (err, data) => {
     if (err) {
       res.writeHead(404);
       res.end(JSON.stringify(err));
